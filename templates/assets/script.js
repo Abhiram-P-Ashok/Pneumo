d13e77bbe23311837fe16da1d3fc38908a962b68
Abhiram P Ashok <93904779+Abhiram-P-Ashok@users.noreply.github.com>
2024-01-07 11:17:26 +0530

    format json&assets

---
+++ templates/assets/script.js
@@ -0,0 +1,20 @@
+function uploadAndPredict() {
+    var formData = new FormData($('#uploadForm')[0]);
+
+    $.ajax({
+        type: 'POST',
+        url: '/',  // Change this to the correct route
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            // Update the prediction and image on the same page
+            $('#predictionText').text('Prediction: ' + data.prediction);
+            $('#uploadedImage').attr('src', 'C:\\Users\\PC\\OneDrive\\Desktop\\pneumonia detection\\static\\uploads\\' + data.filename);
+            $('#predictionResult').show();
+        },
+        error: function (error) {
+            console.log('Error:', error);
+        }
+    });
+}
