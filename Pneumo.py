496257fb5e441822446dafc1c9382bfc80902333
Abhiram P Ashok <93904779+Abhiram-P-Ashok@users.noreply.github.com>
2024-01-23 00:37:39 +0530

    ooh yeah

--- Pneumo.py
+++ Pneumo.py
@@ -1,3 +1,28 @@
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+import sys
+
+
+class WebAppViewer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.webview = QWebEngineView()
+        self.webview.setUrl(QUrl('https://pneumo.pythonanywhere.com'))
+
+        self.setCentralWidget(self.webview)
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle('Web App Viewer')
+        self.setWindowIcon(QIcon('Icons8-Ios7-Healthcare-Lungs.ico'))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = WebAppViewer()
+    window.show()
+    sys.exit(app.exec_())
+    
 '''from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -22,7 +47,7 @@ class WebApp(QMainWindow):
 
         # Create a web browser widget
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://127.0.0.1:5000")) 
+        self.browser.setUrl(QUrl("https://pneumo.pythonanywhere.com")) 
 
         # Set the web browser widget as the central widget
         self.setCentralWidget(self.browser)
@@ -42,39 +67,5 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     QApplication.setApplicationName("Pneumo")  # Replace with your app name
     window = WebApp()
-    app.exec_()'''
-
-'''from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-import sys
-import subprocess
-import os
-
-class WebApp(QMainWindow):
-    def __init__(self):
-        super(WebApp, self).__init__()
-
-        # Execute your app.py script
-        self.execute_app_script()
-
-        # Create a web browser widget
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://127.0.0.1:5000"))  # Replace with your Flask app URL
-
-        # Set the web browser widget as the central widget
-        self.setCentralWidget(self.browser)
-        self.showMaximized()
-
-    def execute_app_script(self):
-        # Replace 'app.py' with the actual name of your Flask app script
-        script_path = os.path.abspath('C:\\Users\\PC\\OneDrive\\Desktop\\pneumonia_detection\\project\\Pneumo\\app.py')
-
-        # Execute the app.py script
-        subprocess.Popen(['python', script_path], shell=True)
-
-app = QApplication(sys.argv)
-QApplication.setApplicationName("Pneumo")  # Replace with your app name
-window = WebApp()
-app.exec_()
+    app.exec_()
 '''
